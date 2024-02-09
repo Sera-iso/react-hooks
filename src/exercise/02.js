24c012116738a662eaaee768fbda0c660b7d5bb3
Serena Isone <74925057+Sera-iso@users.noreply.github.com>
2024-02-09 18:39:57 +0100

    use useEffect and useRef

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -3,14 +3,35 @@
 
 import * as React from 'react'
 
-function Greeting({initialName = ''}) {
-  const localStorageName = window.localStorage.getItem('name')
-  const [name, setName] = React.useState(localStorageName || initialName)
-
-  React.useEffect(() => {
-    window.localStorage.setItem('name', name)
+function useLocalStorageState(
+  key, 
+  defaultValue = '', 
+  {serialize = JSON.stringify, deserialize = JSON.parse} = {},
+) {
+  const [state, setState] = React.useState(() => {
+    const valueInLocalStorage = window.localStorage.getItem('key')
+    if (valueInLocalStorage) {
+      return deserialize(valueInLocalStorage)
+    }
+    return typeof defaultValue === 'function' ? defaultValue() : defaultValue
   })
 
+  const prevKeyRef = React.useRef(key)
+
+  React.useEffect(() => {
+    const prevKey = prevKeyRef.current
+    if (prevKey !== key) {
+      window.localStorage.removeItem(prevKey)
+    }
+    prevKey.current = key
+    window.localStorage.setItem('key', serialize(state))
+  }, [key, serialize, state])
+
+  return [state, setState]
+}
+
+function Greeting({initialName = ''}) {
+  const [name, setName] = useLocalStorageState('name', initialName)
   function handleChange(event) {
     setName(event.target.value)
   }
